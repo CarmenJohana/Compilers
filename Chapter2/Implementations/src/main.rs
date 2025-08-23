442e204bca42d236cb8b5256e929af05a783f9de
CarmenJohana <124937926+CarmenJohana@users.noreply.github.com>
2025-08-23 00:12:57 -0500

    Creation of folders

---
+++ Chapter2/Implementations/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    let x = x + 1;
+
+    {   
+
+        let x = x * 3;
+        println!("The value of x is: {x}");
+    }
+
+
+
+    println!("The value of x is: {x}");
+
+}
+

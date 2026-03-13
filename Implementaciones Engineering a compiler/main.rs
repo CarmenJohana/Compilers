245c3068683823cc8c666032bb5c074d0ab44c67
CarmenJohana <124937926+CarmenJohana@users.noreply.github.com>
2026-03-13 00:49:52 -0500

    Update main.rs

--- Implementaciones Engineering a compiler/main.rs
+++ Implementaciones Engineering a compiler/main.rs
@@ -2,10 +2,19 @@ use std::{collections::HashMap, hash::Hash};
 
 struct Node{
     
-	acceptation: bool,
 	transitions: HashMap<String, Vec<usize>>, // direct transitions by character
     e_transitions: Vec<usize>, // epsilon transitions
 }
+
+impl Node {
+    fn new() -> Self {
+        Node {
+            transitions: HashMap::new(),
+            e_transitions: Vec::new(),
+        }
+    }
+}
+
 
 struct Automata{
 
@@ -62,6 +71,205 @@ impl Automata{
 
     }
 
+	fn symbol_nfa(symbol: &str) -> Automata {
+
+		let mut s0 = Node::new();
+		let s1 = Node::new();
+
+		s0.transitions.insert(symbol.to_string(), vec![1]);
+
+		Automata {
+			states: vec![s0, s1],
+			alphabet: vec![symbol.to_string()],
+			accept_states: vec![1],
+		}
+	}
+
+	fn concatenate(mut a: Automata, mut b: Automata) -> Automata {
+
+    let offset = a.states.len();
+
+    for st in &mut b.states {
+        for v in st.transitions.values_mut() {
+            for x in v {
+                *x += offset;
+            }
+        }
+        for e in &mut st.e_transitions {
+            *e += offset;
+        }
+    }
+
+		for acc in &a.accept_states {
+			a.states[*acc].e_transitions.push(offset);
+		}
+
+		a.states.extend(b.states);
+
+		Automata {
+			states: a.states,
+			alphabet: [a.alphabet, b.alphabet].concat(),
+			accept_states: b.accept_states.iter().map(|x| x + offset).collect(),
+		}
+	}
+
+
+	fn union(mut a: Automata, mut b: Automata) -> Automata {
+
+		let mut start = Node::new();
+
+		let offset_a = 1;
+		let offset_b = 1 + a.states.len();
+
+		start.e_transitions.push(offset_a);
+		start.e_transitions.push(offset_b);
+
+		for st in &mut a.states {
+			for v in st.transitions.values_mut() {
+				for x in v {
+					*x += offset_a;
+				}
+			}
+			for e in &mut st.e_transitions {
+				*e += offset_a;
+			}
+		}
+
+		for st in &mut b.states {
+			for v in st.transitions.values_mut() {
+				for x in v {
+					*x += offset_b;
+				}
+			}
+			for e in &mut st.e_transitions {
+				*e += offset_b;
+			}
+		}
+
+		let mut states = vec![start];
+		states.extend(a.states);
+		states.extend(b.states);
+
+		let mut accept = Vec::new();
+		accept.extend(a.accept_states.iter().map(|x| x + offset_a));
+		accept.extend(b.accept_states.iter().map(|x| x + offset_b));
+
+		Automata {
+			states,
+			alphabet: [a.alphabet, b.alphabet].concat(),
+			accept_states: accept,
+		}
+	}
+
+
+	fn kleene(mut a: Automata) -> Automata {
+
+		let mut start = Node::new();
+		let offset = 1;
+
+		start.e_transitions.push(offset);
+
+		for st in &mut a.states {
+			for v in st.transitions.values_mut() {
+				for x in v {
+					*x += offset;
+				}
+			}
+			for e in &mut st.e_transitions {
+				*e += offset;
+			}
+		}
+
+		for acc in &a.accept_states {
+			a.states[*acc].e_transitions.push(offset);
+		}
+
+		let mut states = vec![start];
+		states.extend(a.states);
+
+		Automata {
+			states,
+			alphabet: a.alphabet,
+			accept_states: vec![0],
+		}
+	}
+
+
+	fn split_top_level(regex: &str, op: char) -> Vec<String> {
+
+		let mut parts = Vec::new();
+		let mut level = 0;
+		let mut last = 0;
+
+		for (i, c) in regex.char_indices() {
+
+			if c == '(' { level += 1; }
+			if c == ')' { level -= 1; }
+
+			if c == op && level == 0 {
+				parts.push(regex[last..i].to_string());
+				last = i + 1;
+			}
+		}
+
+		parts.push(regex[last..].to_string());
+		parts
+	}	
+
+	fn construir(regex: &str) -> Automata {
+
+		let regex = regex.trim();
+
+		// Union
+		let parts = split_top_level(regex, '|');
+		if parts.len() > 1 {
+
+			let mut aut = construir(&parts[0]);
+
+			for p in &parts[1..] {
+				aut = union(aut, construir(p));
+			}
+
+			return aut;
+		}
+
+		// Kleene
+		if regex.ends_with('*') {
+
+			let inner = &regex[..regex.len() - 1];
+			return kleene(construir(inner));
+		}
+
+		// Parentheses
+		if regex.starts_with('(') && regex.ends_with(')') {
+			return construir(&regex[1..regex.len() - 1]);
+		}
+
+		// Concatenation
+		if regex.len() > 1 {
+
+			let mut chars = regex.chars();
+			let first = chars.next().unwrap().to_string();
+
+			let mut aut = symbol_nfa(&first);
+
+			for c in chars {
+				aut = concatenate(aut, symbol_nfa(&c.to_string()));
+			}
+
+			return aut;
+		}
+
+		// symbol
+		symbol_nfa(regex)
+	}
+
+
+
+
+
+
+
 	fn subset_construction(&self) -> Automata{
 		let map_e_clousures = self.e_clousure();
 		let q_0: Vec<usize> = map_e_clousures.get(&0).unwrap().clone();
@@ -84,7 +292,7 @@ impl Automata{
 			
 			
 			let mut new_node = Node{
-				acceptation: false,
+				
 				transitions: HashMap::<String, Vec<usize>>::new(),
 				e_transitions: Vec::<usize>::new(),
 			};
@@ -134,7 +342,7 @@ impl Automata{
 		
 
 	}
-
+	/******
 	fn regex_to_automaton(regex: &str) -> Automata{
 
 
@@ -155,30 +363,32 @@ impl Automata{
 		};
 
 		let parts = regex.split('|');
-		let mut n: usize = 0;
+		let mut n: usize = 1;
 
 		// start.e_transitions.push(n+1); No se puede hacer
 		
 
 		for part in parts {
-		n+=1;
-		aut.states[0].e_transitions.push(n - 1);
-		
-		//let mut INI_CHAR = "";
-		/***
-		let mut current_state: Node = Node{
 
-		acceptation: false,
-		transitions: HashMap::from([(String:.from(), Vec::<usize>::from())]),
-		e_transitions:Vec::<usize>::from([]),
+			aut.states[0].e_transitions.push(n);
+			
+			
+			
+			//let mut INI_CHAR = "";
+			/***
+			let mut current_state: Node = Node{
 
-		};
-		***/
+			acceptation: false,
+			transitions: HashMap::from([(String:.from(), Vec::<usize>::from())]),
+			e_transitions:Vec::<usize>::from([]),
 
-		// let mut INI_PAR = false;
-		let mut open_par: Vec<usize> = Vec::new();
+			};
+			***/
 
-		for c in part.chars(){
+			// let mut INI_PAR = false;
+			let mut open_par: Vec<usize> = Vec::new();
+
+			for c in part.chars(){
 
 
 
@@ -197,6 +407,7 @@ impl Automata{
 
 							
 			aut.states.push(new_state);
+			n+=1;
 
 			}
 			else if c == '('{
@@ -231,12 +442,12 @@ impl Automata{
 			***/
 			if let Some(&number) = open_par.last() {
 
-				if let Some(key) = aut.states[number].transitions.keys().next() {
+				if let Some(key) = aut.states[number-1].transitions.keys().next() {
 
 					let new_state: Node = Node {
 
 						acceptation: false,
-						transitions: HashMap::from([(key.clone(), vec![number+1])]),
+						transitions: HashMap::from([(key.clone(), vec![number-1])]),
 						e_transitions: Vec::new(),
 
 					};
@@ -255,8 +466,8 @@ impl Automata{
 
 
 		}
-			n+=1;
-		}
+			
+	}
 
 					
 
@@ -264,7 +475,7 @@ impl Automata{
 
 return aut
 
-}
+}*****/
 
 }
 
@@ -277,61 +488,61 @@ fn main(){
 	//Estoy creando el automata del libro (a(b | c))
 	//-----------------------------------------------
 	let n_0: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([(String::from("a"), Vec::<usize>::from([1]))]),	
 		e_transitions: Vec::<usize>::from([]),
 	};
 	
 	let n_1: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([]),  
 		e_transitions: Vec::<usize>::from([2]),
 	}; 
 
 	let n_2: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([]),  
 		e_transitions: Vec::<usize>::from([3, 9]),
 	}; 
 
 	let n_3: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([]),  
 		e_transitions: Vec::<usize>::from([4, 6]),
 	}; 
 
 	let n_4: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([(String::from("b"), Vec::<usize>::from([5]))]),  
 		e_transitions: Vec::<usize>::from([]),
 	}; 
 
 	let n_5: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([]),  
 		e_transitions: Vec::<usize>::from([8]),
 	}; 
 
 	let n_6: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([(String::from("c"), Vec::<usize>::from([7]))]),  
 		e_transitions: Vec::<usize>::from([]),
 	}; 
 
 	let n_7: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([]),  
 		e_transitions: Vec::<usize>::from([8]),
 	}; 
 
 	let n_8: Node = Node{
-		acceptation: false,
+		
 		transitions: HashMap::from([]),  
 		e_transitions: Vec::<usize>::from([3, 9]),
 	}; 
 	
 	let n_9: Node = Node{
-		acceptation: true,
+		
 		transitions: HashMap::from([]),  
 		e_transitions: Vec::<usize>::from([]),
 	}; 
